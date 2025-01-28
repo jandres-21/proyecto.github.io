c85e849704fc682272d8a35babf99a279ab402b3
JuanUzhca <juan.uzhca@ucuenca.edu.ec>
2025-01-28 18:22:22 -0500

    Revert "temp actual v4"
    This reverts commit 0149f71d3f1b95cc8994be630942b189f3dea8c6.

--- my-app/routers/router_home.py
+++ my-app/routers/router_home.py
@@ -8,32 +8,95 @@ from controllers.funciones_home import connectionBD
 # Importando conexión a BD
 from controllers.funciones_home import *
 
-# Función para obtener la temperatura actual
-def obtenerTemperaturaActual():
-    try:
-        # Establecer conexión a la base de datos
-        connection = connectionBD()
-        
-        if connection.is_connected():
-            cursor = connection.cursor(dictionary=True)  # Para obtener los resultados como diccionario
-            
-            # Consulta SQL para obtener el último registro de temperatura actual
-            query = "SELECT temperatura FROM temperatura_actual ORDER BY fecha DESC LIMIT 1;"
-            cursor.execute(query)
-            
-            # Obtener el resultado
-            resultado = cursor.fetchone()  # Retorna un solo registro
-            cursor.close()  # Cerrar cursor
-            connection.close()  # Cerrar conexión
-            
-            # Retornar la temperatura si existe, sino retornar None
-            return resultado['temperatura'] if resultado else None
-    
-    except mysql.connector.Error as error:
-        print(f"Error al obtener la temperatura actual: {error}")
-        return None
+@app.route('/lista-de-areas', methods=['GET'])
+def lista_areas():
+    if 'conectado' in session:
+        return render_template('public/usuarios/lista_areas.html', areas=lista_areasBD(), dataLogin=dataLoginSesion())
+    else:
+        flash('Primero debes iniciar sesión.', 'error')
+        return redirect(url_for('inicio'))
 
-# Función para obtener datos de temperatura (paginado)
+@app.route("/lista-de-usuarios", methods=['GET'])
+def usuarios():
+    if 'conectado' in session:
+        return render_template('public/usuarios/lista_usuarios.html', resp_usuariosBD=lista_usuariosBD(), dataLogin=dataLoginSesion(), areas=lista_areasBD(), roles=lista_rolesBD())
+    else:
+        return redirect(url_for('inicioCpanel'))
+
+# Ruta especificada para eliminar un usuario
+@app.route('/borrar-usuario/<string:id>', methods=['GET'])
+def borrarUsuario(id):
+    resp = eliminarUsuario(id)
+    if resp:
+        flash('El Usuario fue eliminado correctamente', 'success')
+        return redirect(url_for('usuarios'))
+
+@app.route('/borrar-area/<string:id_area>/', methods=['GET'])
+def borrarArea(id_area):
+    resp = eliminarArea(id_area)
+    if resp:
+        flash('El Empleado fue eliminado correctamente', 'success')
+        return redirect(url_for('lista_areas'))
+    else:
+        flash('Hay usuarios que pertenecen a esta área', 'error')
+        return redirect(url_for('lista_areas'))
+
+@app.route("/descargar-informe-accesos/", methods=['GET'])
+def reporteBD():
+    if 'conectado' in session:
+        return generarReporteExcel()
+    else:
+        flash('Primero debes iniciar sesión.', 'error')
+        return redirect(url_for('inicio'))
+
+@app.route("/reporte-accesos", methods=['GET'])
+def reporteAccesos():
+    if 'conectado' in session:
+        userData = dataLoginSesion()
+        return render_template('public/perfil/reportes.html', reportes=dataReportes(), lastAccess=lastAccessBD(userData.get('cedula')), dataLogin=dataLoginSesion())
+
+@app.route("/interfaz-clave", methods=['GET','POST'])
+def claves():
+    return render_template('public/usuarios/generar_clave.html', dataLogin=dataLoginSesion())
+
+@app.route('/generar-y-guardar-clave/<string:id>', methods=['GET','POST'])
+def generar_clave(id):
+    print(id)
+    clave_generada = crearClave()  # Llama a la función para generar la clave
+    guardarClaveAuditoria(clave_generada, id)
+    return clave_generada
+
+# CREAR AREA
+@app.route('/crear-area', methods=['GET','POST'])
+def crearArea():
+    if request.method == 'POST':
+        area_name = request.form['nombre_area']  # Asumiendo que 'nombre_area' es el nombre del campo en el formulario
+        resultado_insert = guardarArea(area_name)
+        if resultado_insert:
+            # Éxito al guardar el área
+            flash('El Area fue creada correctamente', 'success')
+            return redirect(url_for('lista_areas'))
+        else:
+            # Manejar error al guardar el área
+            return "Hubo un error al guardar el área."
+    return render_template('public/usuarios/lista_areas')
+
+# ACTUALIZAR AREA
+@app.route('/actualizar-area', methods=['POST'])
+def updateArea():
+    if request.method == 'POST':
+        nombre_area = request.form['nombre_area']  # Asumiendo que 'nuevo_nombre' es el nombre del campo en el formulario
+        id_area = request.form['id_area']
+        resultado_update = actualizarArea(id_area, nombre_area)
+        if resultado_update:
+           # Éxito al actualizar el área
+            flash('El área fue actualizada correctamente', 'success')
+            return redirect(url_for('lista_areas'))
+        else:
+            # Manejar error al actualizar el área
+            return "Hubo un error al actualizar el área."
+
+    return redirect(url_for('lista_areas'))
 def obtenerDatosTemperatura(page=1, per_page=15):
     try:
         # Establecer conexión a la base de datos
@@ -60,15 +123,15 @@ def obtenerDatosTemperatura(page=1, per_page=15):
         print(f"Error al obtener los datos de temperatura: {error}")
         return []
 
-# Ruta para mostrar los datos de temperatura
+
+# Modificamos la ruta para manejar la paginación
 @app.route("/temperatura", methods=['GET'])
 def temperatura():
     if 'conectado' in session:
-        # Obtener el valor de la temperatura actual
-        temperatura_actual = obtenerTemperaturaActual()
-
-        # Obtener los datos de temperatura desde la base de datos con paginación
+        # Obtener el número de página desde la URL, por defecto es la página 1
         page = request.args.get('page', 1, type=int)
+        
+        # Obtener los datos de temperatura desde la base de datos con paginación
         datos_temperatura = obtenerDatosTemperatura(page=page)  # Esta función debe devolver los datos de temperatura
         
         # Obtener el total de registros para la paginación
@@ -87,104 +150,77 @@ def temperatura():
                                datos_temperatura=datos_temperatura, 
                                total_paginas=total_paginas, 
                                page=page, 
-                               temperatura_actual=temperatura_actual,  # Pasamos la temperatura actual a la plantilla
                                dataLogin=dataLoginSesion())
     else:
         flash('Primero debes iniciar sesión.', 'error')
         return redirect(url_for('inicio'))
 
-# Ruta para listar áreas
-@app.route('/lista-de-areas', methods=['GET'])
-def lista_areas():
+
+def obtenerDatosSensoresHumo():
+    try:
+        # Establecer conexión a la base de datos
+        connection = connectionBD()
+        
+        if connection.is_connected():
+            cursor = connection.cursor(dictionary=True)  # Para obtener los resultados como diccionario
+            # Consulta SQL para obtener los registros de sensores de humo
+            query = "SELECT * FROM sensores_humo ORDER BY fecha DESC LIMIT 10;"  # Últimos 10 registros
+            cursor.execute(query)
+            
+            # Obtener los resultados
+            datos = cursor.fetchall()
+            cursor.close()  # Cerrar cursor
+            connection.close()  # Cerrar conexión
+            
+            return datos
+    
+    except mysql.connector.Error as error:
+        print(f"Error al obtener los datos de sensores de humo: {error}")
+        return []
+
+def obtenerDatosRFID():
+    try:
+        # Establecer conexión a la base de datos
+        connection = connectionBD()
+        
+        if connection.is_connected():
+            cursor = connection.cursor(dictionary=True)  # Para obtener los resultados como diccionario
+            # Consulta SQL para obtener los registros de tarjetas RFID
+            query = "SELECT * FROM rfid_tarjetas ORDER BY fecha DESC LIMIT 10;"  # Últimos 10 registros
+            cursor.execute(query)
+            
+            # Obtener los resultados
+            datos = cursor.fetchall()
+            cursor.close()  # Cerrar cursor
+            connection.close()  # Cerrar conexión
+            
+            return datos
+    
+    except mysql.connector.Error as error:
+        print(f"Error al obtener los datos de RFID: {error}")
+        return []
+
+
+
+# RUTA PARA MOSTRAR DATOS DE RFID
+@app.route("/rfid", methods=['GET'])
+def rfid():
     if 'conectado' in session:
-        return render_template('public/usuarios/lista_areas.html', areas=lista_areasBD(), dataLogin=dataLoginSesion())
+        # Obtener los datos de RFID desde la base de datos
+        datos_rfid = obtenerDatosRFID()  # Esta función debe devolver los datos de las tarjetas RFID
+        return render_template('public/rfid.html', datos_rfid=datos_rfid, dataLogin=dataLoginSesion())
     else:
         flash('Primero debes iniciar sesión.', 'error')
         return redirect(url_for('inicio'))
 
-# Ruta para listar usuarios
-@app.route("/lista-de-usuarios", methods=['GET'])
-def usuarios():
+# RUTA PARA MOSTRAR DATOS DE SENSORES DE HUMO
+@app.route("/sensores-humo", methods=['GET'])
+def sensores_humo():
     if 'conectado' in session:
-        return render_template('public/usuarios/lista_usuarios.html', resp_usuariosBD=lista_usuariosBD(), dataLogin=dataLoginSesion(), areas=lista_areasBD(), roles=lista_rolesBD())
-    else:
-        return redirect(url_for('inicioCpanel'))
-
-# Ruta para eliminar un usuario
-@app.route('/borrar-usuario/<string:id>', methods=['GET'])
-def borrarUsuario(id):
-    resp = eliminarUsuario(id)
-    if resp:
-        flash('El Usuario fue eliminado correctamente', 'success')
-        return redirect(url_for('usuarios'))
-
-# Ruta para eliminar un área
-@app.route('/borrar-area/<string:id_area>/', methods=['GET'])
-def borrarArea(id_area):
-    resp = eliminarArea(id_area)
-    if resp:
-        flash('El Empleado fue eliminado correctamente', 'success')
-        return redirect(url_for('lista_areas'))
-    else:
-        flash('Hay usuarios que pertenecen a esta área', 'error')
-        return redirect(url_for('lista_areas'))
-
-# Ruta para generar un reporte de accesos
-@app.route("/descargar-informe-accesos/", methods=['GET'])
-def reporteBD():
-    if 'conectado' in session:
-        return generarReporteExcel()
+        # Obtener los datos de sensores de humo desde la base de datos
+        datos_sensores_humo = obtenerDatosSensoresHumo()  # Esta función debe devolver los datos de los sensores de humo
+        return render_template('public/sensor_humo.html', datos_sensores_humo=datos_sensores_humo, dataLogin=dataLoginSesion())
     else:
         flash('Primero debes iniciar sesión.', 'error')
         return redirect(url_for('inicio'))
 
-# Ruta para reporte de accesos
-@app.route("/reporte-accesos", methods=['GET'])
-def reporteAccesos():
-    if 'conectado' in session:
-        userData = dataLoginSesion()
-        return render_template('public/perfil/reportes.html', reportes=dataReportes(), lastAccess=lastAccessBD(userData.get('cedula')), dataLogin=dataLoginSesion())
-
-# Ruta para generar una clave
-@app.route("/interfaz-clave", methods=['GET','POST'])
-def claves():
-    return render_template('public/usuarios/generar_clave.html', dataLogin=dataLoginSesion())
-
-# Ruta para guardar una nueva clave
-@app.route('/generar-y-guardar-clave/<string:id>', methods=['GET','POST'])
-def generar_clave(id):
-    print(id)
-    clave_generada = crearClave()  # Llama a la función para generar la clave
-    guardarClaveAuditoria(clave_generada, id)
-    return clave_generada
-
-# Ruta para crear un área
-@app.route('/crear-area', methods=['GET','POST'])
-def crearArea():
-    if request.method == 'POST':
-        area_name = request.form['nombre_area']  # Asumiendo que 'nombre_area' es el nombre del campo en el formulario
-        resultado_insert = guardarArea(area_name)
-        if resultado_insert:
-            # Éxito al guardar el área
-            flash('El Area fue creada correctamente', 'success')
-            return redirect(url_for('lista_areas'))
-        else:
-            # Manejar error al guardar el área
-            return "Hubo un error al guardar el área."
-    return render_template('public/usuarios/lista_areas')
-
-# Ruta para actualizar un área
-@app.route('/actualizar-area', methods=['POST'])
-def updateArea():
-    if request.method == 'POST':
-        nombre_area = request.form['nombre_area']  # Asumiendo que 'nuevo_nombre' es el nombre del campo en el formulario
-        id_area = request.form['id_area']
-        resultado_update = actualizarArea(id_area, nombre_area)
-        if resultado_update:
-            # Éxito al actualizar el área
-            flash('El área fue actualizada correctamente', 'success')
-            return redirect(url_for('lista_areas'))
-        else:
-            # Manejar error al actualizar el área
-            return "Hubo un error al actualizar el área."
-    return redirect(url_for('lista_areas'))
